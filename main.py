c914e8eb64b6884b5165073f2b0ac328a23b4fd0
Victor-Main <victorltrgh@gmail.com>
2023-06-15 21:11:00 -0300

    Closing todays coding

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import pygame,sys
 class Amendoca(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.image = pygame.Surface([20, 20])
-        self.image.fill((255, 255, 255))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = [pos_x, pos_y]
+        self.sprites = []
 
 # General setup
 pygame.init()
